10304b731f2fa899a9a6b82e9d0f190120c1d421
ahmed <ahmed@server.example.com>
2020-07-15 14:29:21 +0300

    updated

--- models/models.py
+++ models/models.py
@@ -19,7 +19,7 @@ class Wenk(models.Model):
     risk=fields.Selection(RISK_CHOICES,string="Risk")
 
     ''' Fields below belongs to to competitor notepad page
-    added by AhmedNaseem @INTEGRATEDPATH = 11/07/2020
+    added by AhmedNaseem @INTEGRATEDPATH - 11/07/2020
     '''
     competitor_field_1 = fields.Char(string="Competitor Field 1")
     competitor_field_2 = fields.Char(string="Competitor Field 2")
@@ -28,5 +28,12 @@ class Wenk(models.Model):
     competitor_field_5 = fields.Char(string="Competitor Field 5")
     competitor_field_6 = fields.Char(string="Competitor Field 6")
 
+
+    ''' Fields below belongs to Technical and financial proposal
+    segment of the form added by AhmedNaseem @INTEGRATEDPATH - 11/07/202
+    '''
+
+    financial_proposal = fields.Binary("Financil Proposal ",required=True)
+    technical_proposal = fields.Binary("Technical Proposal ",required=True)
 
 
