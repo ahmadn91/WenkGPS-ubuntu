6cdad95aae275f145dec291418f5efc49d5f568f
ahmed <ahmed@server.example.com>
2020-08-21 16:23:25 +0300

    latest

--- models/models.py
+++ models/models.py
@@ -1,12 +1,15 @@
 #-*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-
+from datetime import datetime
 
 
 class Wenk(models.Model):
 
-    _inherit="crm.lead"
+    _inherit = "crm.lead"
+    
+    
+
     RISK_CHOICES = [
         ("competitor","Competitor"),
         ("edgy high management","Edgy Higher Management"),
@@ -16,24 +19,62 @@ class Wenk(models.Model):
     added by AhmedNaseem @INTEGRATEDPATH - 11/07/2020 
     '''
 
-    risk=fields.Selection(RISK_CHOICES,string="Risk")
+    risk = fields.Selection(RISK_CHOICES,string="Risk")
 
     ''' Fields below belongs to to competitor notepad page
     added by AhmedNaseem @INTEGRATEDPATH - 11/07/2020
     '''
-    competitor_field_1 = fields.Char(string="Competitor Field 1")
-    competitor_field_2 = fields.Char(string="Competitor Field 2")
-    competitor_field_3 = fields.Char(string="Competitor Field 3")
-    competitor_field_4 = fields.Char(string="Competitor Field 4")
-    competitor_field_5 = fields.Char(string="Competitor Field 5")
-    competitor_field_6 = fields.Char(string="Competitor Field 6")
+    competitor_notes = fields.Text(translate=True,string="")
+
+    financial_doc = fields.One2many("wenk.fdocuments","doc_id") # name of the model
+    technical_doc = fields.One2many("wenk.tdocuments","doc_id") 
+    competitor_notes = fields.One2many("wenk.competitor","info_id")
+    
 
 
-    ''' Fields below belongs to Technical and financial proposal
-    segment of the form added by AhmedNaseem @INTEGRATEDPATH - 11/07/202
-    '''
+                            
+class WenkFinDocuments(models.Model):
 
-    financial_proposal = fields.Binary("Financil Proposal ",required=True)
-    technical_proposal = fields.Binary("Technical Proposal ",required=True)
+   
+   _name = "wenk.fdocuments"
+   #_inherit = ['mail.thread']
 
+   #rel_field= fields.Many2one("crm.lead") -I thought we need it, tf man
+   doc_id = fields.Char(string="DOC ID",readonly=True,invisible=True)
+   name = fields.Char(string="Document Name")
+   upload_date = fields.Date(string="Uploaded at",default=datetime.today(),readonly=True,invisible=True)
+   uploader = fields.Many2one('res.users','Uploader', default=lambda self: self.env.user,readonly=True,invisible=True)
+   comment = fields.Char(string="Comments")
+   document = fields.Binary(string="Document")
+   #notify = fields.Many2one("res.users","User to notify")
+
+#    @api.onchage(notify)
+#    def notify_func(self,notify):
+#     if self.notify:
+#         self.message_post(body="Please see document", partner_ids=self.notify,message_type='notification')
+#     else:
+#         pass
+
+   
+class WenkTechDocuments(models.Model):
+
+   _name = "wenk.tdocuments"
+
+   doc_id = fields.Char(string="DOC ID",readonly=True,invisible=True)
+   name = fields.Char(string="Document Name")
+   upload_date = fields.Date(string="Uploaded at",default=datetime.today(),readonly=True,invisible=True)
+   uploader = fields.Many2one('res.users','Uploader', default=lambda self: self.env.user,readonly=True,invisible=True)
+   comment = fields.Char(string="Comments")
+   document = fields.Binary(string="Document")
+    
+
+
+class WenkCompetitorModel(models.Model):
+   _name = "wenk.competitor"
+
+   info_id = fields.Char(string="INFO ID",readonly=True,invisible=True)
+   content = fields.Char(string="Note")
+   upload_date = fields.Date(string="Date Logged",default=datetime.today(),readonly=True,invisible=True)
+   uploader = fields.Many2one('res.users','Noted By', default=lambda self: self.env.user,readonly=True,invisible=True)
+   
 
